af8250dfebcae26038d4b6a711415482eb51fdde
Marty Jones <murtin.jones@gmail.com>
2017-12-18 10:17:20 -0600

    choose position funcitonality

--- battleship.py
+++ battleship.py
@@ -1,4 +1,31 @@
 # this is james and abigail's battleship project
+
+
+def choosePosition(player_n, board):
+    """
+    This function lets a player choose
+    where on the board to place his/her two ships
+    """
+    print " "
+    print "Player %r: select your first slot number for your ship:" % player_n
+    print " "
+    drawBoard(board)
+    slot_1 = int(raw_input("Slot 1 Position Number: > "))
+    board[slot_1] = "X"
+    print "Player %r: select your second slot number for your ship:" % player_n
+    drawBoard(board)
+    slot_2 = int(raw_input("Slot 2 Position Number: > "))
+    while (slot_1 + 3 != slot_2 and slot_1 - 3 != slot_2 and slot_1 + 1 != slot_2 and slot_1 - 1 != slot_2):
+        print "Invalid choice! Try again!"
+        slot_2 = int(raw_input("Slot 2 Position Number: > "))
+    board[slot_2] = "X"
+    for index, each in enumerate(board):
+        if(each != "X"):
+            board[index] = " "
+        else:
+            board[index] = "SNH"
+
+
 def checkIfHitOrMiss(guess, opponents_board):
     """
     This function will check whether a guess
@@ -18,7 +45,7 @@ def checkIfHitOrMiss(guess, opponents_board):
                 print "its the opponents turn"
                 return
         print "you sunk my battleship!"
-        
+
 
 
 
